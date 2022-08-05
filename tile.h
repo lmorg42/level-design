df859c10057405b7f2c7c248b0051d73a2515e9b
lmorg42 <morganl@deakin.edu.au>
2022-08-05 19:02:29 +1000

    Version 2.01

--- tile.h
+++ tile.h
@@ -8,16 +8,15 @@ class Tile
         int value  = 0;
         drawing_options opts;
         rectangle border;
-        int tile_num;
-
+        int tile_size;
 
     public:
-        Tile (bitmap image, point_2d position, int tile_num)
+        Tile (bitmap image, point_2d position, int tile_size)
         {
             this->image = image;
             this->position = position;
-            this->border = rectangle_from(this->position, 64, 64);
-            this->tile_num = tile_num;
+            this->tile_size = tile_size;
+            this->border = rectangle_from(this->position, this->tile_size, this->tile_size);
             this->opts = option_defaults();
             this->opts.draw_cell = 0;
         };
