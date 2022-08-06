95fb84d1b77fb0f37f791089e0c423f5e1f935ff
lmorg42 <morganl@deakin.edu.au>
2022-08-06 16:30:31 +1000

    Version 3

--- program.cpp
+++ program.cpp
@@ -7,6 +7,7 @@
 #define SCREEN_HEIGHT 896
 
 //Make these divisible by (16/32/64) depending on tile size used
+//Gives extra size to the screen, for use with non-static camera
 #define SCREEN_WIDTH_EXTRA 0
 #define SCREEN_HEIGHT_EXTRA 0
 
@@ -29,16 +30,25 @@ int main()
     load_resource_bundle("game_resources", "gameresources.txt");
     open_window("Level Design", SCREEN_WIDTH + SCREEN_WIDTH_EXTRA, SCREEN_HEIGHT + SCREEN_HEIGHT_EXTRA);
 
-    bitmap image = bitmap_named("SewerBlocks");
-    vector<vector<Tile>> tiles;
+    bitmap blocks = bitmap_named("SewerBlocks");
+    string type = "block";
+    //bitmap image = bitmap_named("16TestBlocks");
+    //bitmap image = bitmap_named("32TestBlocks");
+    bitmap pipes = bitmap_named("pipeTiles");
+    bitmap current_selection = blocks;
+    bool draw_pipes = false;
+
     point_2d origin;
     origin.x = 0;
     origin.y = 0;
+
     int tile_size = 64;
     int tile_selection = 1;
 
-    //selection current_selection = DARK_BLOCK;
+    drawing_options opts = option_defaults();
+    opts.draw_cell = 0;
 
+    vector<vector<Tile>> tiles;
     for(int j = 0; j < (SCREEN_HEIGHT + SCREEN_HEIGHT_EXTRA)/tile_size; j++)
     {
         vector<Tile> row;
@@ -46,7 +56,7 @@ int main()
         {
             origin.x = 0 + (i * tile_size);
             origin.y = 0 + (j * tile_size);
-            Tile test(image, origin, tile_size);
+            Tile test(blocks, pipes, origin, tile_size);
             row.push_back(test);
         }
 
@@ -62,7 +72,7 @@ int main()
             for(int j = 0; j < tiles[i].size(); j++)
             {
                 tiles[i][j].draw_tile();
-                tiles[i][j].check_input(tile_selection);
+                tiles[i][j].check_input(tile_selection, type);
             }
         }
 
@@ -71,21 +81,64 @@ int main()
             write_out_level_to_file("file.txt", tiles);
         }
 
+        if(key_typed(P_KEY))
+        {
+            write_line("Switching tiles");
+            if(!draw_pipes)
+            {
+                write_line("Draw Pipes");
+                type = "pipe";
+                draw_pipes = true;
+                tile_selection = 1;
+                current_selection = pipes;
+                opts.draw_cell = 0;
+            }
+            else
+            {
+                write_line("Draw Blocks");
+                type = "block";
+                draw_pipes = false;
+                tile_selection = 1;
+                current_selection = blocks;
+                opts.draw_cell = 0;
+            }
+        }
+
         if(key_typed(UP_KEY))
         {
             tile_selection += 1;
+            opts.draw_cell += 1;
 
-            if(tile_selection > bitmap_cell_count(image))
-                tile_selection = 6;
+            if(tile_selection == 1)
+            {
+                opts.draw_cell = 0;
+            }
+
+            if(tile_selection > bitmap_cell_count(current_selection))
+            {
+                tile_selection = bitmap_cell_count(current_selection);
+                opts.draw_cell = bitmap_cell_count(current_selection) - 1;
+            }
         }
 
         if(key_typed(DOWN_KEY))
         {
             tile_selection -= 1;
+            opts.draw_cell -= 1;
 
             if(tile_selection < 0)
+            {
                 tile_selection = 0;
+                opts.draw_cell = 0;
+            }
         }
+
+        point_2d current_mouse_position = mouse_position();
+
+        if(tile_selection > 0)
+            draw_bitmap(current_selection, current_mouse_position.x, current_mouse_position.y, opts);
+
+
         
         process_events();
         refresh_screen(60);
--- tile.h
+++ tile.h
@@ -3,57 +3,95 @@
 class Tile
 {
     private:
-        bitmap image;
+        bitmap blocks;
+        bitmap pipes;
+        string type;
         point_2d position;
         int value  = 0;
-        drawing_options opts;
+        drawing_options opts_block;
+        drawing_options opts_pipe;
         rectangle border;
         int tile_size;
 
     public:
-        Tile (bitmap image, point_2d position, int tile_size)
+        Tile (bitmap blocks, bitmap pipes, point_2d position, int tile_size)
         {
-            this->image = image;
+            this->blocks = blocks;
+            this->pipes = pipes;
+            this->type = "block";
             this->position = position;
             this->tile_size = tile_size;
             this->border = rectangle_from(this->position, this->tile_size, this->tile_size);
-            this->opts = option_defaults();
-            this->opts.draw_cell = 0;
+            this->opts_block = option_defaults();
+            this->opts_block.draw_cell = 0;
+            this->opts_pipe = option_defaults();
+            this->opts_pipe.draw_cell = 0;
         };
 
         void draw_tile()
         {
-            if(this->value > 0)
-                draw_bitmap(image, position.x, position.y, opts);
+            if(this->value > 0 && this->type == "block")
+                draw_bitmap(blocks, position.x, position.y, opts_block);
+            else if(this->value > 0 && this->type == "pipe")
+                draw_bitmap(pipes, position.x, position.y, opts_pipe);
             
             draw_rectangle(COLOR_GREEN, border);
         };
 
-        void check_input(int tile_selected)
+        void check_input(int tile_selected, string type)
         {
             point_2d pt = mouse_position();
+            drawing_options opts;
+            int multiplier = 1;
 
+            if(type == "block")
+                opts = this->opts_block;
+            else if(type == "pipe")
+            {
+                opts = this->opts_pipe;
+                multiplier = bitmap_cell_count(blocks);
+            }
+
+            opts = update_tile(opts, pt, tile_selected, multiplier, type);
+
+            if(type == "block")
+                this->opts_block = opts;
+            else if(type == "pipe")
+                this->opts_pipe = opts;
+        };
+
+        drawing_options update_tile(drawing_options opts, point_2d mouse_position, int tile_selected, int addition, string type)
+        {
             if(mouse_down(LEFT_BUTTON))
             {
-                if(point_in_rectangle(pt, this->border))
+                if(point_in_rectangle(mouse_position, this->border))
                 {
-                    value = tile_selected;
+                    this->type = type;
+                    this->value = tile_selected + addition;
                     opts.draw_cell = tile_selected - 1;
                 }
             }
-
+            
             if(mouse_down(RIGHT_BUTTON))
             {
-                if(point_in_rectangle(pt, this->border))
+                if(point_in_rectangle(mouse_position, this->border))
                 {
+                    this->type = type;
                     opts.draw_cell = 0;
                     value = 0; 
                 }
             }
+
+            return opts;
         };
 
         int get_value()
         {
             return this->value;
         };
+
+        void set_type(string type)
+        {
+            this->type = type;
+        }
 };
