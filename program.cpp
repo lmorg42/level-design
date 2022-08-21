9f59f77690369aaa5bbd6fae9307c85aed1cbd5d
lmorg42 <morganl@deakin.edu.au>
2022-08-21 12:06:26 +1000

    Added Collectables

--- program.cpp
+++ program.cpp
@@ -99,6 +99,7 @@ int main(int argc, char *argv[])
     cell_sheet_names.push_back("Enemy"); //1400
     cell_sheet_names.push_back("MultiPipes"); //1500
     cell_sheet_names.push_back("EmptyMulti"); //1600
+    cell_sheet_names.push_back("Collect"); //1700
 
     vector<CellSheet> cell_sheets = make_cell_sheets(cell_sheet_names);
 
